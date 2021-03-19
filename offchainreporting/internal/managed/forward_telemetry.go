41e084c38018c07a48cc0d38b59aef3c48c545f0
kaleofduty <59616916+kaleofduty@users.noreply.github.com>
2021-03-19 19:29:51 +0100

    Update forward_telemetry.go

--- offchainreporting/internal/managed/forward_telemetry.go
+++ offchainreporting/internal/managed/forward_telemetry.go
@@ -40,12 +40,12 @@ func forwardTelemetry(
 				logger.Debug("forwardTelemetry: Calling SendLog", types.LogFields{
 					"t": t,
 					"bin": bin,
-				}
+				})
 				monitoringEndpoint.SendLog(bin)
 				logger.Debug("forwardTelemetry: Done calling SendLog", types.LogFields{
 					"t": t,
 					"bin": bin,
-				}					     
+				})				     
 			}
 		case <-ctx.Done():
 			logger.Info("forwardTelemetry: exiting", nil)
