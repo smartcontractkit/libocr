529c2442be4f2ca66d07a6282d076b4d677a0e9a
Domino Valdano <2644901+reductionista@users.noreply.github.com>
2022-10-03 14:46:51 -0700

    Expose Peer interface
    The global function NewPeer() starts and returns a *concretePeer struct,
    but makes it infeasible for the caller to track and later call peer.Close()
    without redefining the concretePeer struct there.  This interface exposes
    the external methods, so that they only need to be defined in one place.

--- networking/types/types.go
+++ networking/types/types.go
@@ -1,6 +1,11 @@
 package types
 
-import "context"
+import (
+	"context"
+
+	ocr1types "github.com/smartcontractkit/libocr/offchainreporting/types"
+	ocr2types "github.com/smartcontractkit/libocr/offchainreporting2/types"
+)
 
 type DiscovererDatabase interface {
 	// StoreAnnouncement has key-value-store semantics and stores a peerID (key) and an associated serialized
@@ -10,4 +15,13 @@ type DiscovererDatabase interface {
 	// ReadAnnouncements returns one serialized announcement (if available) for each of the peerIDs in the form of a map
 	// keyed by each announcement's corresponding peer ID.
 	ReadAnnouncements(ctx context.Context, peerIDs []string) (map[string][]byte, error)
+}
+
+type Peer interface {
+	PeerID() string
+	OCR1BinaryNetworkEndpointFactory() *ocr1types.BinaryNetworkEndpointFactory
+	OCR1BootstrapperFactory() *ocr1types.BootstrapperFactory
+	OCR2BinaryNetworkEndpointFactory() *ocr2types.BinaryNetworkEndpointFactory
+	OCR2BootstrapperFactory() *ocr2types.BootstrapperFactory
+	Close() error
 }
