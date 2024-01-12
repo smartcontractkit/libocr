be676669cd88ed3e1985fc2dce31384d4129bdd5
Jordan Krage <jmank88@gmail.com>
2024-01-12 12:54:47 -0600

    ocr2: add context

--- offchainreporting2plus/types/types.go
+++ offchainreporting2plus/types/types.go
@@ -88,7 +88,7 @@ type ReportingPluginFactory interface {
 	// Creates a new reporting plugin instance. The instance may have
 	// associated goroutines or hold system resources, which should be
 	// released when its Close() function is called.
-	NewReportingPlugin(ReportingPluginConfig) (ReportingPlugin, ReportingPluginInfo, error)
+	NewReportingPlugin(context.Context, ReportingPluginConfig) (ReportingPlugin, ReportingPluginInfo, error)
 }
 
 type ReportingPluginConfig struct {
@@ -327,7 +327,7 @@ type ContractTransmitter interface {
 	)
 
 	// Account from which the transmitter invokes the contract
-	FromAccount() (Account, error)
+	FromAccount(context.Context) (Account, error)
 }
 
 // ContractConfigTracker tracks configuration changes of the OCR contract
